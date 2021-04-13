c0f7d24478336c3def54c1815e1ef375fdb4acb9
Student User <student@workstation.lab.example.com>
2021-04-13 08:05:25 -0400

    New Changes

---
+++ oc-new-app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /usr/local/etc/ocp4.config
+
+oc new-app  --as-deployment-config  --file todo-template.yaml \
+    -p APP_GIT_URL=https://github.com/${RHT_OCP4_GITHUB_USER}/DO288-apps \
+    -p NPM_PROXY=http://${RHT_OCP4_NEXUS_SERVER}/repository/nodejs \
+    -p PASSWORD=mypass \
+    -p CLEAN_DATABASE=true\
+    -p HOSTNAME=${RHT_OCP4_DEV_USER}-todoapp-${RHT_OCP4_WILDCARD_DOMAIN}
+    # ADD MISSING PARAMETERS AND CHANGE PARAMETER VALUES IF NEEDED
